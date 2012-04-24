047f3d7754deaa7e5da945008fe2c0f9b9275035
Tim Jones <ymbirtt@gmail.com>
2012-04-24 18:19:49 +0100

    Renamed parser, lexer, irt and cg, to make compilation easier

--- lexer.hs
+++ lexer.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE CPP #-}
-{-# LINE 1 "C:\Users\Owner\Documents\Haskell Stuff\LEng\lexer.x" #-}
+{-# LINE 1 "Lexer.x" #-}
 
 module Lexer where
 
@@ -122,7 +122,7 @@ alex_deflt :: Array Int Int
 alex_deflt = listArray (0,70) [-1,-1,-1,-1,-1,-1,7,7,7,7,7,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,56,56,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
 
 alex_accept = listArray (0::Int,70) [[],[(AlexAccSkip)],[(AlexAccSkip)],[],[],[(AlexAccSkip)],[],[],[],[],[],[(AlexAcc (alex_action_2))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_3))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_4))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_5))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_6))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_7))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_8))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_9))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_10))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_11))],[(AlexAcc (alex_action_12))],[(AlexAcc (alex_action_13))],[(AlexAcc (alex_action_14))],[(AlexAcc (alex_action_15))],[(AlexAcc (alex_action_16))],[(AlexAcc (alex_action_17))],[],[(AlexAcc (alex_action_18))],[(AlexAcc (alex_action_19))],[(AlexAcc (alex_action_20))],[],[],[(AlexAcc (alex_action_21))],[(AlexAcc (alex_action_21))],[],[],[],[],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_22))],[(AlexAcc (alex_action_23))],[(AlexAcc (alex_action_24))],[(AlexAcc (alex_action_25))],[(AlexAcc (alex_action_26))],[(AlexAcc (alex_action_27))],[]]
-{-# LINE 40 "C:\Users\Owner\Documents\Haskell Stuff\LEng\lexer.x" #-}
+{-# LINE 40 "Lexer.x" #-}
 
 
 --TODO; remove double quotes from the STRING token
